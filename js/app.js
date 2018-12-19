af81ec3e4a3c7665fc4ad541f38b96bf0a3e28c5
Johnathan Niziol <jniziol@gmail.com>
2018-12-18 19:25:03 -0600

    updates service worker location

--- js/app.js
+++ js/app.js
@@ -1,14 +1,4 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('../sw.js')
-    .then(registration => {
-      console.log('Service Worker is registered', registration);
-    })
-    .catch(err => {
-      console.error('Registration failed:', err);
-    });
-  });
-}
+
 
 const rollButton = document.querySelector('.btn-roll');
 const dice1 = document.querySelector('.dice1');
